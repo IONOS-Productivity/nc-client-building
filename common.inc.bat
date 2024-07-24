9111f9b8734f29a6e5b37511a266b52c43910d37
Christopher Hätty <christopher.haetty@brickmakers.de>
2024-07-24 14:16:06 +0200

    [SES-42][SES-65] Set Craft and Qt Paths

--- common.inc.bat
+++ common.inc.bat
@@ -5,14 +5,14 @@ Rem ****************************************************************************
 set "BUILD_ARCH=%~2"
 set "CMAKE_GENERATOR=Ninja"
 
-if "%BUILD_ARCH%" == "Win32" ( set "CRAFT_PATH=c:\Craft32" )
-if "%BUILD_ARCH%" == "Win32" ( set "QT_PATH=%CRAFT_PATH%" )
+if "%BUILD_ARCH%" == "Win32" ( set "CRAFT_PATH=c:\Craft64" )
 if "%BUILD_ARCH%" == "Win32" ( set "PATH=%CRAFT_PATH%\bin;%CRAFT_PATH%\dev-utils\bin;%PATH%" )
-if "%BUILD_ARCH%" == "Win32" ( set "QT_BIN_PATH=%CRAFT_PATH%\bin" )
-if "%BUILD_ARCH%" == "Win32" ( set "QT_PREFIX=%CRAFT_PATH%" )
+if "%BUILD_ARCH%" == "Win32" ( set "QT_PATH=C:\Qt" )
+if "%BUILD_ARCH%" == "Win32" ( set "QT_BIN_PATH=C:\Qt\5.15.2\msvc2019_64\bin" )
+if "%BUILD_ARCH%" == "Win32" ( set "QT_PREFIX=C:\Qt" )
 
 if "%BUILD_ARCH%" == "Win64" ( set "CRAFT_PATH=c:\Craft64" )
-if "%BUILD_ARCH%" == "Win64" ( set "QT_PATH=%CRAFT_PATH%" )
-if "%BUILD_ARCH%" == "Win64" ( set "PATH=%CRAFT_PATH%\bin;%CRAFT_PATH%\dev-utils\bin;%PATH%" )
-if "%BUILD_ARCH%" == "Win64" ( set "QT_BIN_PATH=%CRAFT_PATH%\bin" )
-if "%BUILD_ARCH%" == "Win64" ( set "QT_PREFIX=%CRAFT_PATH%" )
+if "%BUILD_ARCH%" == "Win64" ( set "PATH=%CRAFT_PATH%\bin;%PATH%" )
+if "%BUILD_ARCH%" == "Win64" ( set "QT_PATH=C:\Qt" )
+if "%BUILD_ARCH%" == "Win64" ( set "QT_BIN_PATH=C:\Qt\5.15.2\msvc2019_64\bin" )
+if "%BUILD_ARCH%" == "Win64" ( set "QT_PREFIX=C:\Qt" )
